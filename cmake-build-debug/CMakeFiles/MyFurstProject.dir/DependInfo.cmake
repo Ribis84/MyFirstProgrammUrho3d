602881ee9d4b2be4ff47a6253c9918c73ea83b33
Ribis84 <ivanmik80@gmail.com>
2020-07-21 16:13:29 +0700

    Добавление основных файлов проекта

---
+++ cmake-build-debug/CMakeFiles/MyFurstProject.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/irbis/Urho3D/MyFirstProject/Urho2DSprite.cpp" "/home/irbis/Urho3D/MyFirstProject/cmake-build-debug/CMakeFiles/MyFurstProject.dir/Urho2DSprite.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "URHO3D_ANGELSCRIPT"
+  "URHO3D_IK"
+  "URHO3D_LOGGING"
+  "URHO3D_LUA"
+  "URHO3D_NAVIGATION"
+  "URHO3D_NETWORK"
+  "URHO3D_PHYSICS"
+  "URHO3D_PROFILING"
+  "URHO3D_STATIC_DEFINE"
+  "URHO3D_THREADING"
+  "URHO3D_URHO2D"
+  "URHO3D_WEBP"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/irbis/Urho3D/Urho3D-master/cmake-build-debug/include"
+  "/home/irbis/Urho3D/Urho3D-master/cmake-build-debug/include/Urho3D/ThirdParty"
+  "/home/irbis/Urho3D/Urho3D-master/cmake-build-debug/include/Urho3D/ThirdParty/Bullet"
+  "/home/irbis/Urho3D/Urho3D-master/cmake-build-debug/include/Urho3D/ThirdParty/Lua"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MyFurstProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFurstProject.dir/Urho2DSprite.cpp.o"
+  "bin/MyFurstProject"
+  "bin/MyFurstProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFurstProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
